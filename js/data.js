abebbd588f767c62e4cee90df7c2d8fd8460e513
Kim KJ <rudwnd33@naver.com>
2021-07-21 18:49:34 +0900

    psychological test

---
+++ js/data.js
@@ -0,0 +1,165 @@
+/*
+mouse 0
+cow 1
+tiger 2
+rabbit 3
+dragon 4
+snake 5
+horse 6
+sheep 7
+monkey 8
+chick 9
+dog 10
+pig 11
+*/
+
+const qnaList = [
+    {
+      q: '1. Question 1',
+      a: [
+        { answer: 'Answer 1', type: ['1', '2', '4', '9'] },
+        { answer: 'Answer 2', type: ['0', '3', '6', '5', '10', '8'] },
+        { answer: 'Answer 3', type: ['7', '11' ] },
+      ]
+    },
+    {
+      q: '2. Question 2',
+      a: [
+        { answer: 'Answer 1', type: ['0', '3', '2', '8'] },
+        { answer: 'Answer 2', type: ['1', '6', '5', '10'] },
+        { answer: 'Answer 3', type: ['7', '4', '9', '11'] },
+      ]
+    },
+    {
+      q: '3. Question 3',
+      a: [
+        { answer: 'Answer 1', type: ['1', '3', '2', '10', '8'] },
+        { answer: 'Answer 2', type: ['7', '9', '11'] },
+        { answer: 'Answer 3', type: ['0', '6', '5', '4'] }
+      ]
+    },
+    {
+      q: '4. Question 4',
+      a: [
+        { answer: 'Answer 1" ', type: ['1', '2', '4' ] },
+        { answer: 'Answer 2', type: ['7', '10', '8', '9', '11' ] },
+        { answer: 'Answer 3', type: ['0', '3', '6', '5' ] },
+      ]
+    },
+    {
+      q: '5. Question 5',
+      a: [
+        { answer: 'Answer 1', type: ['1', '7', '10', '4', '9' ] },
+        { answer: 'Answer 2', type: ['0', '3', '6', '11']},
+        { answer: 'Answer 3', type: ['2', '5', '8'] },
+      ]
+    },
+  
+    {
+      q: '6. Question 6',
+      a: [
+        { answer: 'Answer 1', type: ['4', '9', '11' ] },
+        { answer: 'Answer 2', type: ['0', '3', '6', '10' ] },
+        { answer: 'Answer 3', type: ['1', '7', '2', '5', '8' ] },
+      ]
+    },
+    {
+      q: '7. Question 7',
+      a: [
+        { answer: 'Answer 1', type: ['1', '7', '11' ] },
+        { answer: 'Answer 2', type: ['2', '4', '9' ] },
+        { answer: 'Answer 3', type: ['0', '3', '6', '5', '10', '8' ] },
+      ]
+    },
+    {
+      q: '8. Question 8',
+      a: [
+        { answer: 'Answer 1', type: ['0', '4', '9' ] },
+        { answer: 'Answer 2', type: ['3', '2', '6', '5', '10', '8' ] },
+        { answer: 'Answer 3', type: ['1', '7', '11' ] },
+      ]
+    },
+    {
+      q: '9. Question 9',
+      a: [
+        { answer: 'Answer 1', type: ['7', '0', '5', '9' ] },
+        { answer: 'Answer 2', type: ['1', '3', '6', '11' ] },
+        { answer: 'Answer 3', type: ['2', '10', '8', '4' ] },
+      ]
+    },
+    {
+      q: '10. Question 10',
+      a: [
+        { answer: 'Answer 1', type: ['4', '9', '11'] },
+        { answer: 'Answer 2', type: ['0', '2', '6', '5' ] },
+        { answer: 'Answer 3', type: ['1', '7', '3', '10', '8' ] },
+      ]
+    },
+    {
+      q: '11. Question 11',
+      a: [
+        { answer: 'Answer 1', type: ['1', '4', '9', '11' ] },
+        { answer: 'Answer 2', type: ['7', '0', '3', '6', '10'] },
+        { answer: 'Answer 3', type: ['2', '5', '8' ] },
+      ]
+    },
+    {
+      q: '12. Question 12',
+      a: [
+        { answer: 'Answer 1', type: ['3', '6', '4', '9' ] },
+        { answer: 'Answer 2', type: ['0', '2', '5', '10', '8'] },
+        { answer: 'Answer 3', type: ['1', '7', '11'] },
+      ]
+    }
+  ]
+  
+  const infoList = [
+    {
+      name: 'A type <A>',
+      desc: 'Description about A'
+    },
+    {
+      name: 'B type <B>',
+      desc: 'Description about B'
+    },
+    {
+      name: 'C type <C>',
+      desc: 'Description about C'
+    },
+    {
+      name: 'D type <D>',
+      desc: 'Description about D'
+    },
+    {
+      name: 'E type <E>',
+      desc: 'Description about E'
+    },
+    {
+      name: 'F type <F>',
+      desc: 'Description about F'
+    },
+    {
+      name: 'G type <G>',
+      desc: 'Description about G'
+    },
+    {
+      name: 'H type <H>',
+      desc: 'Description about H'
+    },
+    {
+      name: 'I type <I>',
+      desc: 'Description about I'
+    },
+    {
+      name: 'J type <J>',
+      desc: 'Description about J'
+    },
+    {
+      name: 'K type <K>',
+      desc: 'Description about K'
+    },
+    {
+      name: 'L type <L>',
+      desc: 'Description about L'
+    },
+  ]
